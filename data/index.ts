d2783ec173aa5f8721e3939bb6b2725baf916a9d
Christal Bell <christalvictoriabell@gmail.com>
2025-02-04 23:00:49 -0800

    initial with zustand

---
+++ data/index.ts
@@ -0,0 +1,13 @@
+export const perfume = {
+  title: "Vera Wang Princess Eau de Toilette",
+  size: "3.4 fl oz",
+  originalPrice: 28.74,
+  salePrice: 24.42,
+  description:
+    "Vera Wang Princess Perfume will make a lovely addition to your collection with its light blend. It includes a mixture of water lily, apple, mandarin meringue, golden apricot skin, dark chocolate, amber, musk, and vanilla. It comes in an elegant bottle that will stand out on shelves and counter tops. It will make a sweet gift for a friend or loved one who enjoys wearing perfume during the day.",
+  fragranceNotes: {
+    topNotes: "Apple, Water Lily, Mandarin Meringue",
+    middleNotes: "Golden Apricot Skin, Tahitian Tiare Flower, Dark Chocolate",
+    baseNotes: "Vanilla Chiffon, Amber, and Musk",
+  },
+};
---
+++ store/index.ts
@@ -0,0 +1,24 @@
+import { perfume } from "@/data";
+import { create } from "zustand";
+
+interface PerfumeState {
+  perfume: typeof perfume;
+  addToCart: (product: typeof perfume) => void;
+}
+
+const usePerfumeStore = create<PerfumeState>()((set) => ({
+  perfume: {
+    title: "",
+    size: "",
+    originalPrice: 0,
+    salePrice: 0,
+    description: "",
+    fragranceNotes: {
+      topNotes: "",
+      middleNotes: "",
+      baseNotes: "",
+    },
+  },
+  addToCart: (product) => set((state) => ({ perfume: product })),
+  // removeFromCart: (product) => set((state) => ({ perfume: product - 1 })),
+}));
