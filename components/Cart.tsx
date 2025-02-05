fa75a6c8704047c713c251aba27c38cfcfe4d5d8
Christal Bell <christalvictoriabell@gmail.com>
2025-02-05 00:02:51 -0800

    function is complete no styles

--- components/Cart.tsx
+++ components/Cart.tsx
@@ -2,11 +2,25 @@
 import Box from "@mui/material/Box";
 import React from "react";
 import { usePerfumeStore } from "@/store";
+import { Typography } from "@mui/material";
 
 const Cart = () => {
   const perfume = usePerfumeStore((state) => state.perfume);
   console.log(perfume);
-  return <Box>Cart</Box>;
+  return (
+    <Box>
+      Cart
+      {perfume.salePrice === 0 ? (
+        <Typography>Click add to cart</Typography>
+      ) : (
+        <Box>
+          <Typography>{perfume.title}</Typography>
+          <Typography>{perfume.size}</Typography>
+          <Typography>{perfume.salePrice}</Typography>
+        </Box>
+      )}{" "}
+    </Box>
+  );
 };
 
 export default Cart;
